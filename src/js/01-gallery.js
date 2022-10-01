66c71d8605d963535bd0da01eea20e02aba62bf0
Chillbeffore <105280998+Chillbeffore@users.noreply.github.com>
2022-10-02 02:01:28 +0300

    1-2task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+
+const newEl = galleryItems
+  .map(
+    el =>
+      `<a class="gallery__item" href="${el.original}">
+    <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+  </a>`
+  )
+  .join('');
+
+listEl.innerHTML = newEl;
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+  gallery.options.captionsData = 'alt';
+  gallery.options.captionsDelay = '250ms';
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import * as _ from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
+
+function checkLocal() {
+  return localStorage.getItem(CURRENT_TIME) === null
+    ? 0
+    : localStorage.getItem(CURRENT_TIME);
+}
+checkLocal();
+
+player.setCurrentTime(checkLocal());
+
+const saveSett = player.on(
+  'timeupdate',
+  _.throttle(el => {
+    console.log(el.seconds);
+    localStorage.setItem(CURRENT_TIME, el.seconds);
+  }, 1000)
+);
